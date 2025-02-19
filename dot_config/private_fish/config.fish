a3fe9f015dbf7d6f6efaa5d0977f324231741a70
Adrián Berges Enfedaque <aberges@MacBook-Pro-de-Adrian.local>
2025-02-19 23:12:00 +0100

    some changes to ghostty

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux XDF_CONFIG_HOME "$HOME/.config"
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
 set -Ux EDITOR hx
 
 starship init fish | source

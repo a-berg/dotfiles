7ed82a3f029b248a2b65af0181d0e60c8ad78213
Adrián Berges Enfedaque <aberges@MacBook-Pro-de-Adrian.local>
2025-02-26 16:52:59 +0100

    configured ghostty, python for helix, some PATH addons to fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,5 +5,8 @@ end
 set -Ux XDG_CONFIG_HOME "$HOME/.config"
 set -Ux EDITOR hx
 
+fish_add_path -Ua ~/.ghcup/bin/
+fish_add_path -Ua ~/.local/bin/
+
 starship init fish | source
 zoxide init fish | source
